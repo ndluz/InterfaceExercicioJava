4778f31175f0fd2723a5c855d34432e4264f431b
Andy <anderson54644@gmail.com>
2024-03-10 21:39:49 -0300

    refact payment method and interest method to fix calc error

--- src/services/PayPalService.java
+++ src/services/PayPalService.java
@@ -1,13 +1,18 @@
 package services;
 
 public class PayPalService implements OnlinePaymentSerivce {
+	
+	public PayPalService() {
+		
+	}
+	
 	@Override
 	public Double paymentFee(Double amount) {
-		return amount * 0.02;
+		return (amount * 0.02) + amount ;
 	}
 	
 	@Override
 	public Double interest(Double amount, Integer month) {
-		return amount * 0.01 * month;
+		return ((amount * 0.01) * month) + amount;
 	}
 }
